aa3a3dfbe2369ba65a965975c8f0dae50a58a7cd
Ali <alibilge@yahoo.ca>
2020-05-05 02:32:59 -0600

    Final commit

--- footer.php
+++ footer.php
@@ -5,6 +5,12 @@
 <?php echo date( 'Y' ); // Use PHP to keep the year current! less work ?>
 TECHCareers by Manpower
 </p>
+<?php if ( function_exists( 'output_hello_world')) : ?>
+
+<aside>
+<?php output_hello_world(); ?>
+</aside>
+<?php endif; ?>
 </footer>
 <?php wp_footer(); ?>
 </body>
